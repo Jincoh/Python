5da22b86f3dba9d98b4e885b093053892da3a99b
Jinco <jincohgame@gmail.com>
2025-06-15 01:31:54 +0100

    added usage statement to imgur downloader

--- src/imgur_downloader/main.py
+++ src/imgur_downloader/main.py
@@ -1,6 +1,8 @@
-"""Im not downloading all results bc I value my storage, minor adjustments required to change this
-also disclaimer: I don't have a clue how to use imgur so I'm making the assumption it's normal to
-search by tag there since I can't figure out how to search by title"""
+"""disclaimer: I don't have a clue how to use imgur so I'm making the assumption it's normal to
+search by tag there since I can't figure out how to search by title
+
+also imgur seems to not like people scraping images. this app only kinda works, 
+it gets webps and depends on curl"""
 
 
 import requests, bs4
@@ -15,17 +17,34 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from sys import argv
+
 def main():
+    if len(argv) < 2:
+        print("""
+Usage:
+%s
+ + py main.py [search tag]
+  - py main.py cats
+
+Note:
+  Requires curl""" % ("-" * 45))
+
+
+
+    else:
+        functionality(argv[1])
+    
+def functionality(stag: str):
     options = webdriver.FirefoxOptions()
-    #options.add_argument("-headless")
+    options.add_argument("-headless")
     driver = webdriver.Firefox(options=options)
-    driver.get("https://imgur.com/t/cats")
+    driver.get(f"https://imgur.com/t/{stag}")
 #cookies
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".fc-cta-consent"))).click()
     time.sleep(2)
 
     elements = driver.find_elements(By.CSS_SELECTOR, "div[class=imageContainer] img")
-    print(elements)
     num = 1
     for element in elements:
 
